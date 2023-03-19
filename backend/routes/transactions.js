c61497aea97480e15e81786edd4271b914d5300c
CarageaAlexandru <caragea.alexandru@yahoo.com>
2023-03-19 08:45:37 +0000

    moved routes from expenses to transactions becase are from the same category

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -1,4 +1,9 @@
 const {
+	deleteExpense,
+	addExpense,
+	getExpenses,
+} = require("../controllers/expense");
+const {
 	addIncome,
 	getIncomes,
 	deleteIncome,
@@ -9,5 +14,9 @@ const router = require("express").Router();
 router.get("/get-incomes", getIncomes);
 router.post("/add-income", addIncome);
 router.delete("/delete-income/:id", deleteIncome);
+
+router.get("/get-expenses", getExpenses);
+router.post("/add-expense", addExpense);
+router.delete("/delete-expense/:id", deleteExpense);
 
 module.exports = router;
